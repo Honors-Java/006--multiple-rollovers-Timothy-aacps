2a4377d5059eb5ce0fd3bb60785a40b900b76d6e
Timothyaacps <timotheous077@gmail.com>
2020-11-06 15:35:03 +0000

    made the rectangles go black for each quadrant

--- index.java
+++ index.java
@@ -7,12 +7,28 @@ void draw() {
 	stroke(0);
 
   //start creating the lines
-
+  line(00,200,400,200);
+  line(200,00,200,400);
 
   //Fill a black color
-  
 
   //Build your conditional statement 
-	
+	if (mouseX < 200 && mouseY < 200){
+    fill(0);
+    rectMode(CENTER);
+    rect(100,100,200,200);
+  } else if (mouseX < 200 && mouseY > 200){
+    fill(0);
+    rectMode(CENTER);
+    rect(100,300,200,200);
+  } else if (mouseX > 200 && mouseY < 200){
+    fill(0);
+    rectMode(CENTER);
+    rect(300,100,200,200);
+  } else if (mouseX > 200 && mouseY > 200){
+    fill(0);
+    rectMode(CENTER);
+    rect(300,300,200,200);
+  }
 
 }
